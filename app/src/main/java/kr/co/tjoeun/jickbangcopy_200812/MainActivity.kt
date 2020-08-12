540f44a6eddc941c8a2f2f3b6fe6fdd4795dfb6b
이선영 <sunny_525@naver.com>
2020-08-12 21:52:36 +0900

    Main에 방 목록 추가

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_200812/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_200812/MainActivity.kt
@@ -23,6 +23,19 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 //        메인 화면의 값 출력 관련 코드 모아둘 장소
+//        방 목록을 강제로 추가하는 코드
+
+        mRoomList.add(Room(8500, "서울시 은평구", 5, "은평구의 5층 방 입니다."))
+        mRoomList.add(Room(78000, "서울시 은평구", 3, "은평구의 3층 방 입니다."))
+        mRoomList.add(Room(2400, "서울시 은평구", 0, "은평구의 반지하 방 입니다."))
+        mRoomList.add(Room(23000, "서울시 서대문구", -1, "서대문구의 지하1층 방 입니다."))
+        mRoomList.add(Room(175000, "서울시 서대문구", 4, "서대문구의 4층 방 입니다."))
+        mRoomList.add(Room(55000, "서울시 서대문구", 7, "서대문구의 7층 방 입니다."))
+        mRoomList.add(Room(9600, "서울시 동대문구", 0, "동대문구의 반지하 방 입니다."))
+        mRoomList.add(Room(38000, "서울시 동대문구", 2, "동대문구의 2층 방 입니다."))
+        mRoomList.add(Room(57000, "서울시 동대문구", -2, "동대문구의 지하2층 방 입니다."))
+        mRoomList.add(Room(85000, "서울시 동대문구", 5, "동대문구의 5층 방 입니다."))
+
     }
 
 
