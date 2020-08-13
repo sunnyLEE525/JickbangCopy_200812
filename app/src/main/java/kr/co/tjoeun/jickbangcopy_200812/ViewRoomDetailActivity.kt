a155a65732e77dad04effdf68c3c6bbabd0e0c16
이선영 <sunny_525@naver.com>
2020-08-13 19:56:39 +0900

    방 상세 정보 조회 화면 추가 - 화면만 그려둔 상태

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_200812/ViewRoomDetailActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.jickbangcopy_200812
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+        setUpEvents()
+        setValues()
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
